27c49ea2c87648381d454e4e66c487d8929aa088
Zach Smith <elzacho@gmail.com>
2008-07-01 13:00:22 -0600

    Nothing, really, I need to create a copy-imarray function, but haven't figured it out yet.

--- basic.lisp
+++ basic.lisp
@@ -208,6 +208,23 @@ array.  The subspace starts at indices START."
 (defun ima-dimension (arr n)
   (nth n (ima-dims arr)) )
 
+;; (defun copy-imarray (array &key 
+;;                      (element-type (array-element-type array))
+;;                      (fill-pointer (and (array-has-fill-pointer-p array)
+;;                                         (fill-pointer array)))
+;;                      (adjustable (adjustable-array-p array)))
+;;   "Returns an undisplaced copy of ARRAY, with same fill-pointer
+;; and adjustability (if any) as the original, unless overridden by
+;; the keyword arguments."
+;;   (let ((dims (array-dimensions array)))
+;;     ;; Dictionary entry for ADJUST-ARRAY requires adjusting a
+;;     ;; displaced array to a non-displaced one to make a copy.
+;;     (adjust-array
+;;      (make-array dims 
+;;                  :element-type element-type :fill-pointer fill-pointer
+;;                  :adjustable adjustable :displaced-to array)
+;;      dims)))
+
 (defun imarray<-array (arr)
   (construct-index-mapped-array
    (make-array (apply #'* (array-dimensions arr)) :displaced-to arr)
