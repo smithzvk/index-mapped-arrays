9f3f40a59de518688773ecb2bb2504693077cf32
Zach Kost-Smith <elzacho@gmail.com>
2011-03-21 12:20:18 -0600

    Removed commented cruft

--- package.lisp
+++ package.lisp
@@ -16,48 +16,5 @@
            #:make-ima #:copy-ima
            ;; Iterate extensions
            #:in-column-vectors-of #:in-row-vectors-of
-           #:in-ima )
-  ;; (:export
-;;    ;; General mapping generators
-;;            #:array-hyperplane-mapping
-;;            #:vector-mapping
-;;            #:submatrix-mapping
-;;            #:row-major-mapping
-;;            ;; Convenience functions
-;;            #:copy-ima
- )
+           #:in-ima ))
 
-;; (defpackage :index-mapped-arrays
-;;   (:use :cl :tb :alexandria :defclass-star)
-;;   (:shadow #:with-gensyms #:shuffle)
-;;   (:nicknames :ima)
-;;   (:export #:index-mapped-array
-;;            #:imref
-;;            #:ima-dimensions
-;;            #:ima-dimension
-;;            ;; Apply maps
-;;            #:remap-indices
-;;            #:map-indices
-;;            ;; General mapping generators
-;;            #:identity-map
-;;            #:array-hyperplane-mapping
-;;            #:vector-mapping
-;;            #:submatrix-mapping
-;;            #:row-major-mapping
-;;            ;; Convenience functions
-;;            #:get-vector
-;;            #:get-slice
-;;            #:get-subarray
-;;            #:column-vector
-;;            #:row-vector
-;;            ;; Lisp array conversions
-;;            #:imarray<-array
-;;            #:array<-imarray
-;;            ;; Tree/list conversions
-;;            #:tree<-imarray
-;;            #:copy-ima
-;;            ;; Matrix ops (optimized 2D arrays)
-;;            #:row-vector
-;;            #:column-vector
-;;            #:submatrix
-;;            #:transpose ))
