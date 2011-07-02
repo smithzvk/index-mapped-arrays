c1cd9c0aa8ddaf45c4a80048f59d196f53279ea3
Zach Kost-Smith <zachkostsmith@gmail.com>
2011-07-02 01:01:18 -0600

    Exported grouping map symbols and dimensionality raising symbols

--- package.lisp
+++ package.lisp
@@ -6,12 +6,17 @@
            #:immod
            #:ima-dimension #:ima-dimensions
            #:backend-of #:map-of #:data-of #:index-mapped-array
+           #:self-map #:contents-of
+           ;; Getting pieces of IMAs
            #:get-vector #:column-vector #:row-vector
            #:get-diagonal
            #:get-slice
            #:get-block #:submatrix #:subvector
            #:transpose
-           #:self-map #:contents-of
+           ;; Raising the dimensionality
+           #:add-index #:raise-dimensionality #:group-by
+           ;; Grouping IMAs
+           #:group-imas
            #:unmap #:unmap-into #:def-unmapper
            #:make-ima-like #:copy-ima
            ;; Iterate extensions
